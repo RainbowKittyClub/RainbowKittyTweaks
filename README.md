0b5a32b34c43c130e99a81041dbdc772c02f1b5e
K. Gadberry <1781520+kgadberry@users.noreply.github.com>
2025-09-20 04:46:15 +0000

    added nametag recipe

---
+++ README.md
@@ -0,0 +1,6 @@
+**A collection of small Fabric mods for the Rainbow Kitty Club server.**
+
+# Recipe Tweaks
+
+### Name Tag Recipe
+TODO: insert image here
---
+++ src/main/java/club/rainbowkitty/tweaks/Main.java
@@ -0,0 +1,12 @@
+package club.rainbowkitty.tweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    public static final String MOD_ID = "rainbowkittytweaks";
+
+    @Override
+    public void onInitialize() {
+        // Nothing here yet
+    }
+}
